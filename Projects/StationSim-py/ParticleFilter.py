690a379f752c3b72bd0e8c8a2c48b39896df5fa2
kevinminors <44020606+kevinminors@users.noreply.github.com>
2019-02-22 13:59:50 +0000

    Update ParticleFilter.py

--- Projects/StationSim-py/ParticleFilter.py
+++ Projects/StationSim-py/ParticleFilter.py
@@ -5,33 +5,30 @@ import numpy as np
 from copy import deepcopy
 import matplotlib.pyplot as plt
 
-
-'''
-Multiprocessing Methods
-
-Initial state 
-Set the state of the particles to the state of the
-base model.
-
-Assign agents
-Assign the state of the particles to the 
-locations of the agents.
-
-Step particles
-Step each particle model, assign the locations of the 
-agents to the particle state with some noise, and 
-then use the new particle state to set the location 
-of the agents.
-'''
+# Multiprocessing methods
 def initial_state(particle,self):
+    """
+    Set the state of the particles to the state of the
+    base model.
+    """
     self.states[particle,:] = self.base_model.agents2state()
     return self.states[particle]
     
 def assign_agents(particle,self):
+    """
+    Assign the state of the particles to the 
+    locations of the agents.
+    """		
     self.models[particle].state2agents(self.states[particle])
     return self.models[particle]
 
 def step_particles(particle,self):
+    """
+    Step each particle model, assign the locations of the 
+    agents to the particle state with some noise, and 
+    then use the new particle state to set the location 
+    of the agents.
+    """
     self.models[particle].step()
     self.states[particle] = (self.models[particle].agents2state()
                            + np.random.normal(0, self.particle_std**2, 
@@ -39,43 +36,38 @@ def step_particles(particle,self):
     self.models[particle].state2agents(self.states[particle])
     return self.models[particle], self.states[particle]
 
-'''
-Particle Filter
-
-A particle filter to model the dynamics of the
-state of the model as it develops in time.
-
-Parameters:
-
-'number_of_particles': The number of particles used to simulate the model
-'number_of_iterations': The number of iterations to run the model/particle filter
-'resample_window': The number of iterations between resampling particles
-'agents_to_visualise': The number of agents to plot particles for
-'particle_std': The standard deviation of the noise added to particle states
-'model_std': The standard deviation of the noise added to model observations
-'do_save': Boolean to determine if data should be saved and stats printed
-'do_ani': Boolean to determine if particle filter data should be animated
-    and displayed
-'''
 class ParticleFilter: 
-    
-    # check mean error calculation
-    # Update to only measure a certain percentage of the base model
-    
     '''
-    Initialise Particle Filter
-    
-    Firstly, set all attributes using filter parameters. Set time and
-    initialise base model using model parameters. Initialise particle
-    models using a deepcopy of base model. Determine particle filter 
-    dimensions, initialise all remaining arrays, and set initial
-    particle states to the base model state using multiprocessing. 
+    A particle filter to model the dynamics of the
+    state of the model as it develops in time.
     '''
+    
     def __init__(self, Model, model_params, filter_params):
+        '''
+        Initialise Particle Filter
+            
+        PARAMETERS
+         - number_of_particles:     The number of particles used to simulate the model
+         - number_of_iterations:    The number of iterations to run the model/particle filter
+         - resample_window:         The number of iterations between resampling particles
+         - agents_to_visualise:     The number of agents to plot particles for
+         - particle_std:            The standard deviation of the noise added to particle states
+         - model_std:               The standard deviation of the noise added to model observations
+         - do_save:                 Boolean to determine if data should be saved and stats printed
+         - do_ani:                  Boolean to determine if particle filter data should be animated
+                                    and displayed
         
+        DESCRIPTION
+        Firstly, set all attributes using filter parameters. Set time and
+        initialise base model using model parameters. Initialise particle
+        models using a deepcopy of base model. Determine particle filter 
+        dimensions, initialise all remaining arrays, and set initial
+        particle states to the base model state using multiprocessing. 
+        '''    
         for key, value in filter_params.items():
             setattr(self, key, value)
         self.time = 0
+        self.number_of_iterations = model_params['batch_iterations']
         self.base_model = Model(model_params)
         self.models = list([deepcopy(self.base_model) for _ in range(self.number_of_particles)])  
         self.dimensions = len(self.base_model.agents2state())
@@ -90,18 +82,18 @@ class ParticleFilter:
             self.unique_particles = []
         
         self.states = np.array(pool.starmap(initial_state,list(zip(range(self.number_of_particles),[self]*self.number_of_particles))))
-        #self.states = pool.map(initial_state)
-    '''
-    Step Particle Filter
-    
-    Loop through process. Predict the base model and particles
-    forward. If the resample window has been reached, 
-    reweight particles based on distance to base model and resample 
-    particles choosing particles with higher weights. Then save
-    and animate the data. When done, plot save figures.
-    '''
-    def step(self):
         
+    def step(self):
+        '''
+        Step Particle Filter
+        
+        DESCRIPTION
+        Loop through process. Predict the base model and particles
+        forward. If the resample window has been reached, 
+        reweight particles based on distance to base model and resample 
+        particles choosing particles with higher weights. Then save
+        and animate the data. When done, plot save figures.
+        '''
         while self.time < self.number_of_iterations:
             self.time += 1
             
@@ -115,7 +107,8 @@ class ParticleFilter:
     
                 if self.do_save:
                     self.save()
-                if self.do_ani:
+    
+            if self.do_ani:
                     self.ani()
               
         if self.plot_save:
@@ -124,23 +117,19 @@ class ParticleFilter:
         
         return max(self.mean_errors), np.average(self.mean_errors), max(self.variances), np.average(self.variances)
     
-
-    '''
-    Predict
-    
-    Increment time. Step the base model. Set self as a constant
-    in step_particles and then use a multiprocessing method to step 
-    particle models, set the particle states as the agent 
-    locations with some added noise, and reassign the
-    locations of the particle agents using the new particle
-    states. We extract the models and states from the stepped
-    particles variable.
-    
-    This is the main interaction between the 
-    model and the particle filter.
-    '''
     def predict(self):
+        '''
+        Predict
         
+        DESCRIPTION
+        Increment time. Step the base model. Set self as a constant
+        in step_particles and then use a multiprocessing method to step 
+        particle models, set the particle states as the agent 
+        locations with some added noise, and reassign the
+        locations of the particle agents using the new particle
+        states. We extract the models and states from the stepped
+        particles variable.
+        '''
         self.base_model.step()
 
         stepped_particles = pool.starmap(step_particles,list(zip(range(self.number_of_particles),[self]*self.number_of_particles)))
@@ -150,15 +139,16 @@ class ParticleFilter:
         
         return
     
-    '''
-    Reweight
-    
-    Add noise to the base model state to get a measured state. Calculate 
-    the distance between the particle states and the measured base model 
-    state and then calculate the new particle weights as 1/distance. 
-    Add a small term to avoid dividing by 0. Normalise the weights.
-    '''
     def reweight(self):
+        '''
+        Reweight
+        
+        DESCRIPTION
+        Add noise to the base model state to get a measured state. Calculate 
+        the distance between the particle states and the measured base model 
+        state and then calculate the new particle weights as 1/distance. 
+        Add a small term to avoid dividing by 0. Normalise the weights.
+        '''
         measured_state = (self.base_model.agents2state() 
                           + np.random.normal(0, self.model_std**2, size=self.states.shape))
         distance = np.linalg.norm(self.states - measured_state, axis=1)
@@ -166,17 +156,18 @@ class ParticleFilter:
         self.weights /= np.sum(self.weights)
         return
 
-    '''
-    Resample
-    
-    Calculate a random partition of (0,1) and then 
-    take the cumulative sum of the particle weights.
-    Carry out a systematic resample of particles.
-    Set the new particle states and weights and then
-    update agent locations in particle models using 
-    multiprocessing methods.
-    '''
     def resample(self):
+        '''
+        Resample
+        
+        DESCRIPTION
+        Calculate a random partition of (0,1) and then 
+        take the cumulative sum of the particle weights.
+        Carry out a systematic resample of particles.
+        Set the new particle states and weights and then
+        update agent locations in particle models using 
+        multiprocessing methods.
+        '''
         offset_partition = ((np.arange(self.number_of_particles) 
                              + np.random.uniform()) / self.number_of_particles)
         cumsum = np.cumsum(self.weights)
@@ -197,16 +188,16 @@ class ParticleFilter:
 
         return
 
-    '''
-    Save and Plot Save
-    
-    Calculate number of active agents, mean, and variance 
-    of particles and calculate mean error between the mean 
-    and the true base model state. Plot active agents,mean 
-    error and mean variance. 
-    '''
     def save(self):
+        '''
+        Save
         
+        DESCRIPTION
+        Calculate number of active agents, mean, and variance 
+        of particles and calculate mean error between the mean 
+        and the true base model state. Plot active agents,mean 
+        error and mean variance. 
+        '''
         self.active_agents.append(sum([agent.active == 1 for agent in self.base_model.agents]))
         
         active_states = [agent.active == 1 for agent in self.base_model.agents for _ in range(2)]
@@ -224,6 +215,12 @@ class ParticleFilter:
         return
     
     def p_save(self):
+        '''
+        Plot Save
+        
+        DESCRIPTION
+        Plot active agents, mean error and mean variance. 
+        '''
         plt.figure(2)
         plt.plot(self.active_agents)
         plt.ylabel('Active agents')
@@ -249,19 +246,19 @@ class ParticleFilter:
         print('Max mean variance = ',max(self.variances[2:]))
         print('Average mean variance = ',np.average(self.variances[2:]))
 
-    '''
-    Animate
-    
-    Plot the base model state and some of the 
-    particles. Only do this if there is at least 1 active
-    agent in the base model. We adjust the markersizes of
-    each particle to represent the weight of that particle.
-    We then plot some of the agent locations in the particles
-    and draw lines between the particle agent location and 
-    the agent location in the base model. 
-    '''
     def ani(self):
+        '''
+        Animate
         
+        DESCRIPTION
+        Plot the base model state and some of the 
+        particles. Only do this if there is at least 1 active
+        agent in the base model. We adjust the markersizes of
+        each particle to represent the weight of that particle.
+        We then plot some of the agent locations in the particles
+        and draw lines between the particle agent location and 
+        the agent location in the base model. 
+        '''
         if any([agent.active == 1 for agent in self.base_model.agents]):
     
             plt.figure(1)
@@ -293,15 +290,14 @@ class ParticleFilter:
 
 
 def single_run_particle_numbers():
-    particle_num = 10
-    runs = 100
+    particle_num = 100
+    runs = 10
 
     for i in range(runs):
 
         # Run the particle filter
         filter_params = {
             'number_of_particles': particle_num,
-            'number_of_iterations':3000,
             'resample_window': 100,
             'agents_to_visualise': 2,
             'particle_std': 1,
@@ -333,8 +329,8 @@ if __name__ == '__main__':
         'speed_desire_mean': 1,
         'speed_desire_std': 1,
         'separation': 2,
-        'batch_iterations': 900,
-        'do_save': True,
+        'batch_iterations': 4000,
+        'do_save': False,
         'do_ani': False,
         }
     if not True:

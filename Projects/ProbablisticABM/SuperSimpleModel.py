-Added density heat-map to the distribution plot. -Added mean and standard dev to the plot. -Axis(x, y) labels added. -Fixed Scaling issues. y now represent density percentage. -Refactored the plot into its own functions.

Projects/ProbablisticABM/SuperSimpleModel.py
@@ -6,6 +6,15 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 from tensorflow_probability import distributions as tfd
 import matplotlib.pyplot as plt
+from matplotlib import colors
+
+import scipy.stats as stats
+
+# from sys import platform
+# if platform == "linux" or platform == "linux2":
+#     clear = 'clear'
+# elif platform == "win32":
+#     clear = 'cls'
 
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
@@ -16,17 +25,16 @@ except ValueError:
     pass
 
 truth_observation = tfd.Normal(loc=1, scale=1.)
-print(tf.test.is_gpu_available())
-
 
 SIZE = 1000
 EXIT = 100.
 
+# Declare plot outside of loop.
 fig, ax = plt.subplots()
 
 
 def custom_stepper():
-    tog = True
+    truthy = True
 
     # Position
     x = tfd.Normal(loc=1, scale=1.)
@@ -35,39 +43,56 @@ def custom_stepper():
     v = tfd.Exponential(rate=1)
     v_sample = v.sample(SIZE)
     # Noise
-    u = tfd.Normal(loc=0, scale=0.5)
+    u = tfd.Normal(loc=0, scale=1)
 
     clear_output_folder()
 
     t = [[1.0]]
     while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        # Move the agent.
         x_sample = tf.math.add(x_sample, tf.math.multiply(v_sample, tf.Variable(t)))
+        # Add random noise.
         x_sample = tf.math.add(x_sample, u.sample(SIZE))
 
-        os.system('clear')
-        if tog:
-            print('\\Working/')
-            tog = False
-        else:
-            print('/Working\\')
-            tog = True
-
-        print(tf.math.reduce_mean(x_sample).numpy())
-        ax.hist(x_sample, range=[0, EXIT * 1.5], bins=100, label=str(tf.math.reduce_mean(x_sample)))
-        ylim = SIZE * .5
-        ax.set_ylim(0, ylim)
-        ax.set_xlabel('x')
-        ax.set_ylabel('Probability of Location')
-
-        plt.savefig('output/{}.png'.format(time.time()))
-        ax.clear()
+        plot_agent(x_sample)
 
         if tf.cond(pred=tf.greater(tf.math.reduce_mean(x_sample), tf.constant(EXIT)),
                    true_fn=lambda: True,
                    false_fn=lambda: False):
             break
 
+        if truthy:
+            print('\\Working/')
+            truthy = False
+        else:
+            print('/Working\\')
+            truthy = True
+        print(tf.math.reduce_mean(x_sample).numpy())
+
+
     render_agent()
+
+
+def plot_agent(x_sample):
+    n, bins, patches = ax.hist(x_sample, bins=100, density=1)
+    fracs = n / n.max()
+    norm = colors.Normalize(fracs.min(), fracs.max())
+    for thisfrac, thispatch in zip(fracs, patches):
+        color = plt.cm.inferno(norm(thisfrac))
+        thispatch.set_facecolor(color)
+    ax.set_ylim(0, .3)
+    ax.set_xlim(EXIT * -.25, EXIT * 1.5)
+    ax.set_xlabel('x')
+    ax.set_ylabel('Density in Location')
+    ax.set_title(r'Single Agent:  $\mu={}$, $\sigma={}$'.format(format(tf.math.reduce_mean(x_sample).numpy(),
+                                                                       '9.5'),
+                                                                format(tf.math.reduce_std(x_sample).numpy(),
+                                                                       '9.5')))
+    # Save the plot to disk.
+    plt.savefig('output/{}.png'.format(time.time()))
+    # Clear for the next plot.
+    ax.clear()
 
 
 def render_agent():
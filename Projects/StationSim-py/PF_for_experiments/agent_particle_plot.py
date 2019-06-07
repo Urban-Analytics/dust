1cd57c3fe8aee9c18b6cdfcd60c8adee20fb5332
Nick Malleson <n.s.malleson@leeds.ac.uk>
2019-06-07 10:48:16 +0100

    put plots into a loop over a dictionary

--- Projects/StationSim-py/PF_for_experiments/agent_particle_plot.py
+++ Projects/StationSim-py/PF_for_experiments/agent_particle_plot.py
@@ -5,6 +5,8 @@ Created on Thu Apr  4 14:09:12 2019
 @author: medkmin
 """
 
+#%% Initialise and read files
+
 import os
 import re
 import numpy as np
@@ -28,9 +30,13 @@ for r, d, f in os.walk(path):
             
 if len(files) == 0:
     sys.exit("Found no files in {}, can't continue".format(path) )
+else:
+    print("Found {} files".format(len(files)))
     
-particles = [1] + list(range(10,1010,10))
-agents = [1] + list(range(10,310,10))
+particles = [1] + list(range(10,1010,10)) # Particles 10 -> 1000
+agents = [1] + list(range(10,310,10)) # Agents 10 -> 300
+
+# Errors are a matrix of particles * agents
 
 min_mean_err = np.zeros(shape=(len(particles),len(agents)))
 max_mean_err = np.zeros(shape=(len(particles),len(agents)))
@@ -42,6 +48,7 @@ min_var = np.zeros(shape=(len(particles),len(agents)))
 max_var = np.zeros(shape=(len(particles),len(agents)))
 ave_var = np.zeros(shape=(len(particles),len(agents)))
 
+print("Reading files....",)
 for f in files:
 
     file = open(f,"r").read()
@@ -60,7 +67,11 @@ for f in files:
     min_var[particles.index(particle_num),agents.index(agent_num)] = data.mean()[6]
     max_var[particles.index(particle_num),agents.index(agent_num)] = data.mean()[7]
     ave_var[particles.index(particle_num),agents.index(agent_num)] = data.mean()[-1]
-        
+    
+print("...finished reading")
+    
+#%% Plot full data
+
 #plt.figure(1)
 #plt.imshow(min_mean_err, aspect = 'auto', origin = 'lower', extent = [1,300,1,1000])
 #plt.xlabel('Agents')
@@ -77,55 +88,62 @@ for f in files:
 
 #ave_mean_err = ave_mean_err[1:10,:]
 
-plt.figure(3)
-plt.imshow(ave_mean_err, aspect = 'auto', origin = 'lower', extent = [1,300,10,100])
+# Can restrict the number of agents and/or particles to look at in the plots
+# (note this is an index into the actual number of agents/particles)
+min_particles = particles.index(10)
+max_agents = agents.index(100)
+
+
+# Define the plots so that they can be plotted in a loop
+plot_def = {
+    "Min mean error" : min_mean_err,
+    "Max mean error" : max_mean_err,
+    "Avg mean error" : ave_mean_err, 
+    "Min abs error"  : min_abs_err,
+    "Max abs error"  : max_abs_err,
+    "Avg abs error"  : ave_abs_err,
+    "Min variance"   : min_var,
+    "Max variance"   : max_var,
+    "Avg variance:"  : ave_var
+    }
+
+for i, (title, data) in enumerate(plot_def.items()):
+    plt.figure(i)
+    plt.imshow( data[min_particles: , :max_agents ], aspect = 'auto', origin = 'lower', 
+           extent = [min(agents),agents[max_agents], particles[min_particles], max(particles)])
+    plt.xlabel('Agents')
+    plt.ylabel('Particles')
+    plt.title(title)
+    plt.colorbar()
+
+
+
+  
+#%% Plot with few agents
+
+p = 0.2 # Proportion agents to include (only the first x %)
+
+fig, ax = plt.subplots()
+plt.imshow(ave_mean_err[ : , 0:int(p*len(agents))], aspect = 'auto', origin = 'lower')
 plt.xlabel('Agents')
+ax.set_xticklabels(range(0,300,10))
 plt.ylabel('Particles')
 plt.title('Average Mean Error')
 plt.colorbar()
 
-#plt.figure(4)
-#plt.imshow(min_abs_err, aspect = 'auto', origin = 'lower', extent = [1,300,1,1000])
-#plt.xlabel('Agents')
-#plt.ylabel('Particles')
-#plt.title('Min Absolute Error')
-#plt.colorbar()
-#
-#plt.figure(5)
-#plt.imshow(max_abs_err, aspect = 'auto', origin = 'lower', extent = [1,300,1,1000])
-#plt.xlabel('Agents')
-#plt.ylabel('Particles')
-#plt.title('Max Absolute Error')    
-#plt.colorbar()
 
-#ave_abs_err = ave_abs_err[1:10,:]
+
+plt.figure(3)
+#plt.imshow(ave_mean_err, aspect = 'auto', origin = 'lower', extent = [1,300,10,100])
+plt.imshow(ave_mean_err[ : , 0:int(p*len(agents))], aspect = 'auto', origin = 'lower', extent = [1,int(p*300),10,100])
+plt.xlabel('Agents')
+plt.ylabel('Particles')
+plt.title('Average Mean Error')
+plt.colorbar()
         
 plt.figure(6)
-plt.imshow(ave_abs_err, aspect = 'auto', origin = 'lower', extent = [1,300,10,100])
+plt.imshow(ave_abs_err[ : , 0:int(p*len(agents))], aspect = 'auto', origin = 'lower', extent = [1,int(p*300),10,100])
 plt.xlabel('Agents')
 plt.ylabel('Particles')
 plt.title('Average Absolute Error')        
 plt.colorbar()
-    
-#plt.figure(7)
-#plt.imshow(min_var, aspect = 'auto', origin = 'lower', extent = [1,300,1,1000])
-#plt.xlabel('Agents')
-#plt.ylabel('Particles')
-#plt.title('Min Variance')
-#plt.colorbar()
-#
-#plt.figure(8)
-#plt.imshow(max_var, aspect = 'auto', origin = 'lower', extent = [1,300,1,1000])
-#plt.xlabel('Agents')
-#plt.ylabel('Particles')
-#plt.title('Max Variance')
-#plt.colorbar()
-#
-#plt.figure(9)
-#plt.imshow(ave_var, aspect = 'auto', origin = 'lower', extent = [1,300,1,1000])
-#plt.xlabel('Agents')
-#plt.ylabel('Particles')
-#plt.title('Average Variance')            
-#plt.colorbar()    
-  
-    

432b0e6d60ef0c39aa756ecb67f2a08c2238e497
Nick Malleson <n.s.malleson@leeds.ac.uk>
2019-06-15 09:19:14 +0100

    renamed particle filter experiments directory and updated references in the python code

--- Projects/StationSim-py/Particle_Filter_Experiments/agent_particle_plot.py
+++ Projects/StationSim-py/Particle_Filter_Experiments/agent_particle_plot.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Reads the out files that are created by StationSim-ARCExperiments.py
-This version accounts for two types of results being written for each experiment: before and after reweighting.
-
 Created on Thu Apr  4 14:09:12 2019
 
-@author: medkmin (adapted by Nick Malleson)
+@author: medkmin
 """
 
 #%% Initialise and read files
@@ -19,16 +16,7 @@ import sys
 
 # Needs to be set to location of results
 #path = 'M:\Particle Filter\Model Results\HPC results\With noise = 10'
-path = "/Users/nick/gp/dust/Projects/StationSim-py/PF_for_experiments/results/FewAgentExperiments/"
-
-# Model the errors before or after resampling? 0 = before, 1= after
-before = 0
-print("Calculating errors {} resampling:".format('before' if before==0 else 'after'))
-
-# Need to set the number of particles and agents used in the experiments 
-# (these are set in StationSim-ARCExperiments.py)
-particles  = list(range(1,49,1))  + list(range(50,501,50)) + list(range(600,2001,100)) + list(range(2500,4001,500))
-agents = list(range(1,21,1))
+path = "/Users/nick/gp/dust/Projects/StationSim-py/Particle_Filter_Experiments/results/noise-02"
 
 if not os.path.isdir(path):
     sys.exit("Directory '{}' does not exist".format(path))
@@ -45,7 +33,8 @@ if len(files) == 0:
 else:
     print("Found {} files".format(len(files)))
     
-
+particles = [1] + list(range(10,1010,10)) # Particles 10 -> 1000
+agents = [1] + list(range(10,310,10)) # Agents 10 -> 300
 
 # Errors are a matrix of particles * agents
 
@@ -64,33 +53,45 @@ for f in files:
 
     file = open(f,"r").read()
     data = pd.read_csv(f, header = 2).replace('on',np.nan)
-    #data.iloc[:,0] = pd.to_numeric(data.iloc[:,0]) # Not sure why this was necessary
-    
-    # Filter by whether errors are before or after
-    data = data[ data.loc[:,'Before_resample?'] == before]
+    data.iloc[:,0] = pd.to_numeric(data.iloc[:,0])
     
     particle_num = int(re.findall('particles\': (\d{1,4})',file)[0])
     agent_num = int(re.findall('pop_total\': (\d{1,3})',file)[0])
-    data_mean = data.mean() # Calculate the mean of all columns
     
-    min_mean_err[particles.index(particle_num),agents.index(agent_num)] = data_mean['Min_Mean_errors']
-    max_mean_err[particles.index(particle_num),agents.index(agent_num)] = data_mean['Max_Mean_errors']
-    ave_mean_err[particles.index(particle_num),agents.index(agent_num)] = data_mean['Average_mean_errors']
-    min_abs_err[particles.index(particle_num),agents.index(agent_num)] = data_mean['Min_Absolute_errors']
-    max_abs_err[particles.index(particle_num),agents.index(agent_num)] = data_mean['Max_Absolute_errors']
-    ave_abs_err[particles.index(particle_num),agents.index(agent_num)] = data_mean['Average_Absolute_errors']
-    min_var[particles.index(particle_num),agents.index(agent_num)] = data_mean['Min_variances']
-    max_var[particles.index(particle_num),agents.index(agent_num)] = data_mean['Max_variances']
-    ave_var[particles.index(particle_num),agents.index(agent_num)] = data.mean()['Average_variances']
+    min_mean_err[particles.index(particle_num),agents.index(agent_num)] = data.mean()[0]
+    max_mean_err[particles.index(particle_num),agents.index(agent_num)] = data.mean()[1]
+    ave_mean_err[particles.index(particle_num),agents.index(agent_num)] = data.mean()[2]
+    min_abs_err[particles.index(particle_num),agents.index(agent_num)] = data.mean()[3]
+    max_abs_err[particles.index(particle_num),agents.index(agent_num)] = data.mean()[4]
+    ave_abs_err[particles.index(particle_num),agents.index(agent_num)] = data.mean()[5]
+    min_var[particles.index(particle_num),agents.index(agent_num)] = data.mean()[6]
+    max_var[particles.index(particle_num),agents.index(agent_num)] = data.mean()[7]
+    ave_var[particles.index(particle_num),agents.index(agent_num)] = data.mean()[-1]
     
 print("...finished reading")
     
 #%% Plot full data
 
+#plt.figure(1)
+#plt.imshow(min_mean_err, aspect = 'auto', origin = 'lower', extent = [1,300,1,1000])
+#plt.xlabel('Agents')
+#plt.ylabel('Particles')
+#plt.title('Min Mean Error')
+#plt.colorbar()
+
+#plt.figure(2)
+#plt.imshow(max_mean_err, aspect = 'auto', origin = 'lower', extent = [1,300,1,1000])
+#plt.xlabel('Agents')
+#plt.ylabel('Particles')
+#plt.title('Max Mean Error')
+#plt.colorbar()
+
+#ave_mean_err = ave_mean_err[1:10,:]
+
 # Can restrict the number of agents and/or particles to look at in the plots
 # (note this is an index into the actual number of agents/particles)
 min_particles = particles.index(10)
-max_agents = agents.index(agents[len(agents)-1]) # agents[len(agents)-1] gives all agents
+max_agents = agents.index(50)
 
 
 # Define the plots so that they can be plotted in a loop
@@ -103,7 +104,7 @@ plot_def = {
     "Avg abs error"  : ave_abs_err,
     "Min variance"   : min_var,
     "Max variance"   : max_var,
-    "Avg variance"  : ave_var
+    "Avg variance:"  : ave_var
     }
 
 for i, (title, data) in enumerate(plot_def.items()):
@@ -112,7 +113,37 @@ for i, (title, data) in enumerate(plot_def.items()):
            extent = [min(agents),agents[max_agents], particles[min_particles], max(particles)])
     plt.xlabel('Agents')
     plt.ylabel('Particles')
-    plt.title(title+" ({} resampling)".format('before' if before==0 else 'after') )
+    plt.title(title)
     plt.colorbar()
 
 
+
+  
+#%% Plot with few agents
+
+p = 0.2 # Proportion agents to include (only the first x %)
+
+fig, ax = plt.subplots()
+plt.imshow(ave_mean_err[ : , 0:int(p*len(agents))], aspect = 'auto', origin = 'lower')
+plt.xlabel('Agents')
+ax.set_xticklabels(range(0,300,10))
+plt.ylabel('Particles')
+plt.title('Average Mean Error')
+plt.colorbar()
+
+
+
+plt.figure(3)
+#plt.imshow(ave_mean_err, aspect = 'auto', origin = 'lower', extent = [1,300,10,100])
+plt.imshow(ave_mean_err[ : , 0:int(p*len(agents))], aspect = 'auto', origin = 'lower', extent = [1,int(p*300),10,100])
+plt.xlabel('Agents')
+plt.ylabel('Particles')
+plt.title('Average Mean Error')
+plt.colorbar()
+        
+plt.figure(6)
+plt.imshow(ave_abs_err[ : , 0:int(p*len(agents))], aspect = 'auto', origin = 'lower', extent = [1,int(p*300),10,100])
+plt.xlabel('Agents')
+plt.ylabel('Particles')
+plt.title('Average Absolute Error')        
+plt.colorbar()

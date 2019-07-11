54b908c8e278550d0d24b44e40e6866aae32253b
Minh <kieuleminh@gmail.com>
2019-07-11 09:45:25 +0100

    commit BusSim to Master

---
+++ Projects/ABM_DA/bussim/A03_calibration.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+"""
+This code evaluates the outputs from calibrated BusSim
+@author: geomlk
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+
+'''
+Step 1: Load calibration results
+'''
+def load_calibrated_params_IncreaseRate(IncreaseRate):
+    name0 = ['/Users/minhlkieu/Dropbox/Minh_UoL/DA/ABM/BusSim/Calibration/BusSim_Model2_calibration_IncreaseRate_',str(IncreaseRate),'.pkl']
+    str1 = ''.join(name0)    
+    with open(str1, 'rb') as f:      
+        model_params, best_mean_model2,Sol_archived_mean,Sol_archived_std,PI_archived = pickle.load(f)
+
+    name0 = ['/Users/minhlkieu/Dropbox/Minh_UoL/DA/ABM/BusSim/Calibration/BusSim_Model1_calibration_IncreaseRate_',str(IncreaseRate),'.pkl']
+    str1 = ''.join(name0)    
+    with open(str1, 'rb') as f:      
+        model_params, best_mean_model1,Sol_archived_mean,Sol_archived_std,PI_archived = pickle.load(f)
+    return best_mean_model1,best_mean_model2    
+
+def load_calibrated_params_maxDemand(maxDemand):
+    name0 = ['/Users/minhlkieu/Dropbox/Minh_UoL/DA/ABM/BusSim/Calibration/BusSim_Model2_calibration_static_maxDemand_',str(maxDemand),'.pkl']
+    str1 = ''.join(name0)    
+    with open(str1, 'rb') as f:      
+        model_params, best_mean_model2,Sol_archived_mean,Sol_archived_std,PI_archived = pickle.load(f)
+
+    name0 = ['/Users/minhlkieu/Dropbox/Minh_UoL/DA/ABM/BusSim/Calibration/BusSim_Model1_calibration_static_maxDemand_',str(maxDemand),'.pkl']
+    str1 = ''.join(name0)    
+    with open(str1, 'rb') as f:      
+        model_params, best_mean_model1,Sol_archived_mean,Sol_archived_std,PI_archived = pickle.load(f)
+    return best_mean_model1,best_mean_model2    
+'''
+Step 2: Load synthetic real-time data
+'''
+def load_actual_params_IncreaseRate(IncreaseRate):
+    #load up a model from a Pickle    
+    name0 = ['/Users/minhlkieu/Dropbox/Minh_UoL/DA/ABM/BusSim/Data/Realtime_data_IncreaseRate_',str(IncreaseRate),'.pkl']
+    str1 = ''.join(name0)    
+
+    with open(str1, 'rb') as f:
+        model_params,t,x,GroundTruth = pickle.load(f)
+    return model_params,t,x
+
+def load_actual_params_maxDemand(maxDemand):
+    #load up a model from a Pickle    
+    name0 = ['/Users/minhlkieu/Dropbox/Minh_UoL/DA/ABM/BusSim/Data/Realtime_data_static_maxDemand_',str(maxDemand),'.pkl']
+    str1 = ''.join(name0)    
+
+    with open(str1, 'rb') as f:
+        model_params,t,x,GroundTruth = pickle.load(f)
+    return model_params,t,x
+#define RMSE function
+def rmse(yhat,y):
+    return np.sqrt(np.square(np.subtract(yhat, y).mean()))
+'''
+Step 3: Evaluation of calibrated models when the arrival rate is changing by 1 to 20%
+'''
+def IncreaseRate_analysis():
+    Results = [0,0]
+    do_plot=True
+    for IncreaseRate in range(1,20,2):
+        #load real-time data
+        model_params, t,x = load_actual_params_IncreaseRate(IncreaseRate)
+        #load calibrated parameters
+        best_mean_model1,best_mean_model2 = load_calibrated_params_IncreaseRate(IncreaseRate)
+        #load the BusSim-static model
+        from BusSim_static_v2 import Model as Model2
+        ArrivalRate = best_mean_model2[0:(model_params['NumberOfStop'])]
+        DepartureRate = best_mean_model2[model_params['NumberOfStop']:(2*model_params['NumberOfStop'])]    
+        TrafficSpeed = best_mean_model2[-2]
+        #load model
+        model = Model2(model_params, TrafficSpeed,ArrivalRate,DepartureRate)
+        for time_step in range(int(model.EndTime / model.dt)):
+            model.step()
+        x2 = np.array([bus.trajectory for bus in model.buses]).T        
+        t2 = np.arange(0, model.EndTime, model.dt)
+        x2[x2 <= 0 ] = np.nan
+        x2[x2 >= (model.NumberOfStop * model.LengthBetweenStop)] = np.nan            
+        
+        #load the BusSim-stochastic model
+        from BusSim_static_deterministic import Model as Model1
+        ArrivalRate = best_mean_model1[0:(model_params['NumberOfStop'])]
+        DepartureRate = best_mean_model1[model_params['NumberOfStop']:(2*model_params['NumberOfStop'])]    
+        TrafficSpeed = best_mean_model1[-2]
+        #load model
+        model = Model1(model_params, TrafficSpeed,ArrivalRate,DepartureRate)
+        for time_step in range(int(model.EndTime / model.dt)):
+            model.step()
+        x3 = np.array([bus.trajectory for bus in model.buses]).T        
+        t3 = np.arange(0, model.EndTime, model.dt)
+        x3[x3 <= 0 ] = np.nan
+        x3[x3 >= (model.NumberOfStop * model.LengthBetweenStop)] = np.nan            
+        #plot individual run (if needed)
+        if do_plot:
+            plt.figure(3, figsize=(16 / 2, 9 / 2))
+            plt.clf()            
+            plt.plot(t, x, linewidth=1,color='black',linestyle = '-')
+            plt.plot(t2, x2, linewidth=1.5,linestyle = ':',color='r')
+            plt.ylabel('Distance (m)')
+            plt.xlabel('Time (s)')
+            plt.plot(t3, x3, linewidth=1.5,linestyle = '--',color='b')
+
+            plt.plot([], [], linewidth=1.5,linestyle = ':',color='r',label='BusSim-stochastic')
+            plt.plot([], [], linewidth=1.5,linestyle = '--',color='b',label='BusSim-deterministic')
+            plt.plot([], [], linewidth=1,color='black',linestyle = '-',label='Real-time')
+    
+            plt.legend()
+            plt.show()
+            name0 = ['/Users/minhlkieu/Dropbox/Minh_UoL/DA/ABM/BusSim/Figures/Fig_calibration_IncreaseRate_',str(IncreaseRate),'.pdf']
+            str1 = ''.join(name0)    
+            plt.savefig(str1, dpi=200,bbox_inches='tight')
+        #calculate RMSE    
+        x3[np.isnan(x3)]=0
+        x2[np.isnan(x2)]=0
+        x[np.isnan(x)]=0
+        RMSE1 = rmse(x3,x)
+        RMSE2 = rmse(x2,x)
+        Results =  np.vstack((Results,[RMSE1,RMSE2]))
+    #plot the evaluation results        
+    do_plot_results=True
+    if do_plot_results:
+        plt.figure(3, figsize=(16 / 2, 9 / 2))
+        plt.clf()            
+        plt.plot(np.arange(1,20,2),Results[1:,1],linewidth=1.5,linestyle = '--',color='b',label='BusSim-deterministic')
+        plt.plot(np.arange(1,20,2),Results[1:,0],linewidth=1.5,linestyle = ':',color='r',label='BusSim-stochastic')
+        plt.ylabel('RMSE (m)')
+        plt.xlabel(r'$\xi$ (%)') 
+        plt.legend()
+        plt.show()
+        plt.savefig('/Users/minhlkieu/Dropbox/Minh_UoL/DA/ABM/BusSim/Figures/Fig_calibration_results_IncreaseRate.pdf', dpi=200,bbox_inches='tight')
+
+    return Results
+'''
+Step 3: Evaluation of the case when the maxDemand increases from 0.5 to 4.5
+'''
+def maxDemand_analysis():
+    Results = [0,0]
+    do_plot=False
+    for maxDemand in range(1,10,2):
+        maxDemand=maxDemand/2
+        model_params, t,x = load_actual_params_maxDemand(maxDemand)
+        best_mean_model1,best_mean_model2 = load_calibrated_params_maxDemand(maxDemand)
+        from BusSim_static_v2 import Model as Model2
+        ArrivalRate = best_mean_model2[0:(model_params['NumberOfStop'])]
+        DepartureRate = best_mean_model2[model_params['NumberOfStop']:(2*model_params['NumberOfStop'])]    
+        TrafficSpeed = best_mean_model2[-1]
+        #load model
+        model = Model2(model_params, TrafficSpeed,ArrivalRate,DepartureRate)
+        for time_step in range(int(model.EndTime / model.dt)):
+            model.step()
+        x2 = np.array([bus.trajectory for bus in model.buses]).T        
+        t2 = np.arange(0, model.EndTime, model.dt)
+        x2[x2 <= 0 ] = np.nan
+        x2[x2 >= (model.NumberOfStop * model.LengthBetweenStop)] = np.nan            
+        from BusSim_static_deterministic import Model as Model1
+        ArrivalRate = best_mean_model1[0:(model_params['NumberOfStop'])]
+        DepartureRate = best_mean_model1[model_params['NumberOfStop']:(2*model_params['NumberOfStop'])]    
+        TrafficSpeed = best_mean_model1[-1]
+        #load model
+        model = Model1(model_params, TrafficSpeed,ArrivalRate,DepartureRate)
+        for time_step in range(int(model.EndTime / model.dt)):
+            model.step()
+        x3 = np.array([bus.trajectory for bus in model.buses]).T        
+        t3 = np.arange(0, model.EndTime, model.dt)
+        x3[x3 <= 0 ] = np.nan
+        x3[x3 >= (model.NumberOfStop * model.LengthBetweenStop)] = np.nan            
+        #plot individual plots if it's needed
+        if do_plot:
+            plt.figure(3, figsize=(16 / 2, 9 / 2))
+            plt.clf()            
+            plt.plot(t2, x2, linewidth=1,linestyle = ':',color='r',label='BusSim-stochastic')
+            plt.plot(t, x, linewidth=1,color='black',linestyle = '-',label='Real-time')
+            plt.ylabel('Distance (m)')
+            plt.xlabel('Time (s)')
+            plt.plot(t3, x3, linewidth=.5,linestyle = '--',color='b',label='BusSim-deterministic')
+            plt.legend()
+            plt.show()
+            name0 = ['/Users/minhlkieu/Dropbox/Minh_UoL/DA/ABM/BusSim/Figures/Fig_calibration_maxDemand_',str(maxDemand),'.pdf']
+            str1 = ''.join(name0)    
+            plt.savefig(str1, dpi=200,bbox_inches='tight')
+        #calculate RMSE for each run    
+        x3[np.isnan(x3)]=0
+        x2[np.isnan(x2)]=0
+        x[np.isnan(x)]=0
+        RMSE1 = rmse(x3,x)
+        RMSE2 = rmse(x2,x)
+        Results =  np.vstack((Results,[RMSE1,RMSE2]))
+    #plot the evaluation results        
+    do_plot_results=True
+    if do_plot_results:
+        plt.figure(3, figsize=(16 / 2, 9 / 2))
+        plt.clf()            
+        plt.plot(np.arange(1,10,2),Results[1:,0],linewidth=1.5,linestyle = '--',color='b',label='BusSim-deterministic')
+        plt.plot(np.arange(1,10,2),Results[1:,1],linewidth=1.5,linestyle = ':',color='r',label='BusSim-stochastic')
+        plt.ylabel('RMSE (m)')
+        plt.xlabel(r'$maxDemand$ (passenger/min)')
+        plt.xticks(np.arange(1,10,2), (np.arange(1,10,2)/2)) 
+        plt.legend()
+        plt.show()
+        plt.savefig('/Users/minhlkieu/Dropbox/Minh_UoL/DA/ABM/BusSim/Figures/Fig_calibration_results_maxDemand.pdf', dpi=200,bbox_inches='tight')
+
+    return Results
+
+
+if __name__ == '__main__':  #main code, just call the evaluation codes
+    
+    Results = IncreaseRate_analysis()
+    #Results = maxDemand_analysis()
---
+++ Projects/ABM_DA/bussim/ParticleFilter_MK.py
@@ -0,0 +1,268 @@
+# Particle Filter
+'''
+A particle filter designed to work with agent base models.
+model requires: agents2state(), state2agents()
+model requests: boundaries, agents[:].active
+step requires: measured_state
+save requests: true_state
+'''
+import numpy as np
+from copy import deepcopy
+import matplotlib.pyplot as plt
+
+class ParticleFilter:
+    '''
+    A particle filter to model the dynamics of the
+    state of the model as it develops in time.
+
+    Parameters:
+
+    'number_of_particles': The number of particles used to simulate the model
+    'number_of_iterations': The number of iterations to run the model/particle filter
+    'resample_window': The number of iterations between resampling particles
+    'agents_to_visualise': The number of agents to plot particles for
+    'particle_std': The standard deviation of the noise added to particle states
+    'model_std': The standard deviation of the noise added to model observations
+    'do_save': Boolean to determine if data should be saved and stats printed
+    'do_ani': Boolean to determine if particle filter data should be animated
+        and displayed
+    '''
+    def __init__(self, model, number_of_particles, arr_std=0,dep_std=0, traffic_std=0, resample_window=10, do_copies=True, do_save=False):
+        '''
+        Initialise Particle Filter
+
+        Firstly, set all attributes using filter parameters. Set time and
+        initialise base model using model parameters. Initialise particle
+        models using a deepcopy of base model. Determine particle filter
+        dimensions, set state of particles to the state of the base model,
+        and then initialise all remaining arrays.
+        '''
+        self.time = 0
+        # Dimensions
+        self.number_of_particles = number_of_particles
+        self.dimensions = len(model.agents2state().T)
+        # Models
+        self.models = list([deepcopy(model) for _ in range(self.number_of_particles)])
+        for unique_id in range(len(self.models)):
+            self.models[unique_id].unique_id = unique_id
+        if not do_copies:
+            # Only relevent if there is randomness in the initialisation of the model.
+            for model in self.models:
+                model.__init__(*model.params)
+        # Filter
+        self.states = np.empty((self.number_of_particles, self.dimensions))
+        for particle in range(self.number_of_particles):
+            self.states[particle] = self.models[particle].agents2state()
+        self.weights = np.ones(self.number_of_particles)
+        # Params
+        self.arr_std = arr_std
+        self.dep_std = dep_std
+        self.traffic_std=traffic_std
+        self.resample_window = resample_window
+        # Save
+        self.do_save = do_save
+        if self.do_save:
+            self.active = []
+            self.means = []
+            self.mean_errors = []
+            self.variances = []
+        return
+
+    def step(self, measured_state, true_state=None):
+        '''
+        Step Particle Filter
+
+        Loop through process. Predict the base model and particles
+        forward. If the resample window has been reached,
+        reweight particles based on distance to base model and resample
+        particles choosing particles with higher weights. Then save
+        and animate the data. When done, plot save figures.
+        '''
+        self.predict(measured_state)
+        self.reweight(measured_state)
+        self.resample(measured_state)
+        if self.do_save:
+            self.save(true_state)
+        return
+
+
+    def reweight(self, measured_state):
+        '''def 
+        Reweight
+
+        Add noise to the base model state to get a measured state. Calculate
+        the distance between the particle states and the measured base model
+        state and then calculate the new particle weights as 1/distance.
+        Add a small term to avoid dividing by 0. Normalise the weights.
+        '''
+        
+        states = self.states[:, :len(measured_state)]  # For shorter measurements to state vectors
+        
+        #print(states)
+        distance = np.linalg.norm(states - measured_state, axis=1)  #Frobenius norm
+        #print(distance)
+        self.weights = 1 / np.fmax(distance, 1e-99)  # to avoid fp_err
+        #self.weights = np.exp(-np.fmax(distance, 1e-99))  # to avoid fp_err
+        self.weights /= np.sum(self.weights)
+        #print(self.weights)
+        return 
+
+    def resample(self,measured_state):  # systematic sampling
+        '''
+        Resample
+
+        Calculate a random partition of (0,1) and then
+        take the cumulative sum of the particle weights.
+        Carry out a systematic resample of particles.
+        Set the new particle states and weights and then
+        update agent locations in particle models.
+        '''
+        if not self.time % self.resample_window:
+            offset = (np.arange(self.number_of_particles) + np.random.uniform()) / self.number_of_particles
+            cumsum = np.cumsum(self.weights)
+            i, j = 0, 0
+            indexes = np.zeros(self.number_of_particles, 'i')
+            while i < self.number_of_particles and j < self.number_of_particles:
+                if offset[i] < cumsum[j]:  # reject
+                    indexes[i] = j
+                    i += 1
+                else:
+                    j += 1
+            self.states = self.states[indexes]
+            #print(self.states)
+            self.weights = self.weights[indexes]
+            
+            self.states[:,len(measured_state)+1:len(measured_state)+self.models[0].FleetSize-1] += np.random.normal(0, self.arr_std, self.states[:,len(measured_state)+1:len(measured_state)+self.models[0].FleetSize-1].shape)
+            self.states[:,len(measured_state)+self.models[0].FleetSize+2:-3] += np.random.normal(0, self.dep_std, self.states[:,len(measured_state)+self.models[0].FleetSize+2:-3].shape)
+            self.states[:,-1] += np.random.normal(0, self.traffic_std, self.states[:,-1].shape)
+
+            #apply the measured_state
+            #for s in range(len(self.states)):
+            #    self.states[s,:len(measured_state)]=measured_state
+        
+        
+        return
+
+    def predict(self,measured_state):
+        '''
+        Predict
+
+        Increment time. Step the base model. For each particle,
+        step the particle model and then set the particle states
+        as the agent locations with some added noise. Reassign the
+        locations of the particle agents using the new particle
+        states.
+
+        This is the main interaction between the
+        model and the particle filter.
+        '''
+       
+        for particle in range(self.number_of_particles):
+            self.models[particle].state2agents(self.states[particle])
+            self.models[particle].step()
+            self.states[particle] = self.models[particle].agents2state()
+        self.time += 1
+        return
+
+    def predict_one(self, particle):  # not working
+        map(self.predict_one, np.arange(self.number_of_particles))
+        self.models[particle].state2agents(self.states[particle])
+        self.models[particle].step()
+        self.states[particle] = self.models[particle].agents2state()
+        return
+
+    def save(self, true_state):
+        '''
+        Save and Plot Save
+
+        Calculate number of active agents, mean, and variance
+        of particles and calculate mean error between the mean
+        and the true base model state. Plot active agents,mean
+        error and mean variance.
+        '''
+        states = self.states[:, :len(true_state)]  # For shorter measurements to state vectors
+
+        try:
+            activity = sum([agent.active for agent in self.model.agents])
+            print('act')
+        except AttributeError:
+            activity = None
+        self.active.append(activity)
+
+        mean = np.average(states, weights=self.weights, axis=0)
+        self.means.append(mean)
+
+        variance = np.average((states - mean)**2, weights=self.weights, axis=0)
+        self.variances.append(np.average(variance))
+
+        if true_state is None:
+            self.mean_errors.append(None)
+        else:
+            self.mean_errors.append(np.linalg.norm(mean - true_state, axis=0))
+        return
+
+    def plot_save(self):
+
+        plt.figure(1)
+        plt.plot(self.active)
+        plt.ylabel('Active agents')
+
+        plt.figure(2)
+        plt.plot(self.mean_errors, '-k')
+        plt.ylabel('Particle Mean Error')
+
+        ax2 = plt.twinx()
+        ax2.plot(self.variances)
+        ax2.set_ylabel('Particle Variance Error')
+
+        plt.show()
+
+        print('Max mean error = ',max(self.mean_errors))
+        print('Average mean error = ',np.average(self.mean_errors))
+        print('Max mean variance = ',max(self.variances))
+        print('Average mean variance = ',np.average(self.variances))
+        return
+
+    def ani(self, model, pf_agents=2):
+        '''
+        Animate
+
+        Plot the base model state and some of the
+        particles. Only do this if there is at least 1 active
+        agent in the base model. We adjust the markersizes of
+        each particle to represent the weight of that particle.
+        We then plot some of the agent locations in the particles
+        and draw lines between the particle agent location and
+        the agent location in the base model.
+        '''
+        plt.figure(1)
+        plt.clf()
+
+        # pf
+        if np.std(self.weights):
+            markersizes = self.weights
+            markersizes *= 4 / np.std(markersizes)    # revar
+            markersizes += 4 - np.mean(markersizes)   # remean
+            markersizes = np.clip(markersizes, 1, 8)  # clip
+        else:
+            markersizes = 8*np.ones(self.weights.shape)
+
+        for unique_id in range(pf_agents):
+            for particle in range(self.number_of_particles):
+
+                loc0 = model.agents[unique_id].location
+                loc = self.models[particle].agents[unique_id].location
+                locs = np.array([loc0, loc]).T
+
+                plt.plot(*locs, '-k', alpha=.1, linewidth=.3)
+                plt.plot(*loc, '+r', alpha=.3, markersize=markersizes[particle])
+
+        # model
+        state = model.agents2state(do_ravel=False).T
+        plt.plot(state[0], state[1], '.k')
+        try:
+            plt.axis(np.ravel(model.boundaries, 'F'))
+        except:
+            pass
+        plt.pause(1/30)
+        return

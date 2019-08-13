63b3598b9b998cbf462b5530cc835fa11def315e
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2019-08-13 14:03:44 +0100

    write function to run enkf multiple times and store data

--- Projects/ABM_DA/experiments/enkf_experiments/main.py
+++ Projects/ABM_DA/experiments/enkf_experiments/main.py
@@ -1,14 +1,20 @@
+"""
+main.py
+@author: ksuchak1990
+Python script for running experiments with the enkf.
+"""
+
+# Imports
+import json
 import sys
+import numpy as np
+from numpy.random import normal
 sys.path.append('../../stationsim/')
 
 from stationsim_model import Model
 from ensemble_kalman_filter import EnsembleKalmanFilter
 
-import numpy as np
-from numpy.random import normal
-
-
-np.random.seed(42)
+# np.random.seed(42)
 
 # Functions
 def make_truth_data(model_params):
@@ -88,6 +94,60 @@ def run_all(pop_size=20, its=300, assimilation_period=50, ensemble_size=10):
     enkf = run_enkf(model_params, filter_params)
     enkf.process_results()
 
+def run_repeat(N=10, write_json=False):
+    """
+    Repeatedly run an enkf realisation of stationsim.
+
+    Run a realisation of stationsim with the enkf repeatedly.
+    Produces RMSE values for forecast, analysis and observations at each
+    assimilation step.
+
+    Parameters
+    ----------
+    N : int
+        The number of times we want to run the ABM-DA.
+    """
+    model_params = {'width': 200,
+                    'height': 100,
+                    'pop_total': 20,
+                    'gates_in': 3,
+                    'gates_space': 2,
+                    'gates_speed': 4,
+                    'gates_out': 2,
+                    'speed_min': .1,
+                    'speed_mean': 1,
+                    'speed_std': 1,
+                    'speed_steps': 3,
+                    'separation': 4,
+                    'max_wiggle': 1,
+                    'step_limit': 300,
+                    'do_history': True,
+                    'do_print': False}
+
+    OBS_NOISE_STD = 1
+    vec_length = 2 * model_params['pop_total']
+
+    filter_params = {'max_iterations': model_params['step_limit'],
+                     'assimilation_period': 50,
+                     'ensemble_size': 10,
+                     'state_vector_length': vec_length,
+                     'data_vector_length': vec_length,
+                     'H': np.identity(vec_length),
+                     'R_vector': OBS_NOISE_STD * np.ones(vec_length),
+                     'keep_results': True,
+                     'vis': False}
+
+    errors = list()
+    for i in range(N):
+        print('Running iteration {0}'.format(i+1))
+        enkf = run_enkf(model_params, filter_params)
+        errors.append(enkf.rmse)
+
+    if write_json:
+        with open('data.json', 'w', encoding='utf-8') as f:
+            json.dump(errors, f, ensure_ascii=False, indent=4)
+    return errors
+
 def run_combos():
     """
     Run the ensemble kalman filter for different combinations of:
@@ -103,5 +163,8 @@ def run_combos():
             combo = (20, 300, a, e)
             run_all(*combo)
 
-run_all(20, 300, 50, 10)
+
+# run_all(20, 300, 50, 10)
 # run_combos()
+# run_repeat()
+

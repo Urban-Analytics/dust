50282e377fca78563c4af0ec3a63f0df5b35f3f4
BenjaminIsaac0111 <benjamintaya0111@gmail.com>
2020-04-01 20:16:40 +0100

    Was meant to do this along time ago. Committed the code with documentation and comments.

--- Projects/ProbabilisticABM/SimpleABM/ExitGate.py
+++
@@ -1,7 +0,0 @@
-from SimpleABM.Object import Object
-
-
-class ExitGate(Object):
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
--- Projects/ProbabilisticABM/SimpleABM/Steppable.py
+++
@@ -1,7 +0,0 @@
-class Steppable:
-
-    def __init__(self, **kwargs):
-        self.attributes = kwargs
-
-    def get_attributes(self):
-        return self.attributes

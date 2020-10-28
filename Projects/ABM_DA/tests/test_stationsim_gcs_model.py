8f1dca5ac6676b1418748627272129ea5a853fec
ksuchak1990 <ksuchak1990@yahoo.co.uk>
2020-10-28 17:13:53 +0100

    Moving tests into correct section

--- Projects/ABM_DA/tests/test_stationsim_gcs_model.py
+++ Projects/ABM_DA/tests/test_stationsim_gcs_model.py
@@ -20,33 +20,6 @@ distance_data = get_distance_data()
 
 
 # Tests
-@pytest.mark.parametrize('agent_locations, expected', get_state_location_data)
-def test_get_state_location(agent_locations, expected):
-    model = set_up_model()
-    for i, agent in enumerate(model.agents):
-        agent.location = agent_locations[i]
-    assert np.array_equal(model.get_state('location'), expected)
-
-
-@pytest.mark.parametrize('agent_locations, expected',
-                         get_state_location2D_data)
-def test_get_state_location2D(agent_locations, expected):
-    model = set_up_model()
-    for i, agent in enumerate(model.agents):
-        agent.location = agent_locations[i]
-    assert model.get_state('location2D') == expected
-
-
-@pytest.mark.parametrize('agent_locations, exits, expected',
-                         get_state_loc_exit_data)
-def test_get_state_loc_exit(agent_locations, exits, expected):
-    model = set_up_model()
-    for i, agent in enumerate(model.agents):
-        agent.location = agent_locations[i]
-        agent.gate_out = exits[i]
-    assert model.get_state('loc_exit') == expected
-
-
 # Model tests
 def test_station_setup():
     model = set_up_model()
@@ -88,6 +61,33 @@ def test_clock_setup():
 
 def test_gate_out_allocation():
     pass
+
+
+@pytest.mark.parametrize('agent_locations, expected', get_state_location_data)
+def test_get_state_location(agent_locations, expected):
+    model = set_up_model()
+    for i, agent in enumerate(model.agents):
+        agent.location = agent_locations[i]
+    assert np.array_equal(model.get_state('location'), expected)
+
+
+@pytest.mark.parametrize('agent_locations, expected',
+                         get_state_location2D_data)
+def test_get_state_location2D(agent_locations, expected):
+    model = set_up_model()
+    for i, agent in enumerate(model.agents):
+        agent.location = agent_locations[i]
+    assert model.get_state('location2D') == expected
+
+
+@pytest.mark.parametrize('agent_locations, exits, expected',
+                         get_state_loc_exit_data)
+def test_get_state_loc_exit(agent_locations, exits, expected):
+    model = set_up_model()
+    for i, agent in enumerate(model.agents):
+        agent.location = agent_locations[i]
+        agent.gate_out = exits[i]
+    assert model.get_state('loc_exit') == expected
 
 
 # Agent tests

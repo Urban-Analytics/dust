0285ec3a1d9a4ca9f6b73c9aba6743e0a3cd063b
ksuchak1990 <ksuchak1990@yahoo.co.uk>
2021-01-25 12:45:53 +0100

    Separate experiments into separate files

--- Projects/ABM_DA/experiments/enkf_experiments/enkf_experiment3.py
+++ Projects/ABM_DA/experiments/enkf_experiments/enkf_experiment3.py
@@ -6,8 +6,9 @@ Python script for running experiments with the enkf.
 
 # Imports
 import numpy as np
-from experiment_utils import Modeller, Processor, Visualiser
+from experiment_utils import Modeller, Visualiser, Processor
 
 np.random.seed(42)
 
-Processor.process_batch()
+# Modeller.run_experiment_1(pop_size=100)
+Processor.process_experiment_1()

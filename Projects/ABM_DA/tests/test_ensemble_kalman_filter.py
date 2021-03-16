08c7593e698d7555b2901f89ecf7fa0a1d3e0367
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2021-03-16 17:03:02 +0100

    Write test for population mean - ensemble min

--- Projects/ABM_DA/tests/test_ensemble_kalman_filter.py
+++ Projects/ABM_DA/tests/test_ensemble_kalman_filter.py
@@ -33,6 +33,8 @@ population_mean_base_data = get_population_mean_base_data()
 
 population_mean_mean_data = get_population_mean_mean_data()
 
+population_mean_min_data = get_population_mean_min_data()
+
 population_mean_max_data = get_population_mean_max_data()
 
 # Tests
@@ -139,8 +141,23 @@ def test_get_population_mean_mean_en(results, truth,
     assert enkf.get_population_mean(results, truth) == expected
 
 
-def test_get_population_mean_min_en():
-    pass
+@pytest.mark.parametrize('results, truth, ensemble_active, expected',
+                         population_mean_min_data)
+def test_get_population_mean_min_en(results, truth,
+                                    ensemble_active, expected):
+    # Set up enkf
+    enkf = set_up_enkf()
+    enkf.error_normalisation = ActiveAgentNormaliser.MIN_EN
+
+    # Define number of active agents for each ensemble member
+    for i, model in enumerate(enkf.models):
+        model.pop_active = ensemble_active[i]
+
+    # Define results and truth values
+    results = np.array(results)
+    truth = np.array(truth)
+
+    assert enkf.get_population_mean(results, truth) == expected
 
 
 @pytest.mark.parametrize('results, truth, ensemble_active, expected',

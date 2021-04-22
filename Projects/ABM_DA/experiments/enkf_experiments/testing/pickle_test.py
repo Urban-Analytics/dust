aac6b11a071be29a389cdd59160a4cb3b4977c88
Keiran Suchak <ksuchak1990@yahoo.co.uk>
2021-04-22 13:49:23 +0100

    Reorganise enkf experiments
    * Move scripts used to testing new tools into their own folder.

--- Projects/ABM_DA/experiments/enkf_experiments/testing/pickle_test.py
+++ Projects/ABM_DA/experiments/enkf_experiments/testing/pickle_test.py
@@ -5,10 +5,10 @@ import pickle
 # x = {'A': [1, 2, 3, 4, 5],
 #      'B': [2, 3, 4, 5, 6]}
 
-# with open('./results/models/test.pkl', 'wb') as f:
+# with open('../results/models/test.pkl', 'wb') as f:
 #     pickle.dump(x, f)
 
-with open('./results/models/test.pkl', 'rb') as f:
+with open('../results/models/test.pkl', 'rb') as f:
     x = pickle.load(f)
 
 print(x)
